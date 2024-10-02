167d8790378e864a78d29293b9e7656eae1b9309
karim tarek <karim.tarek.t201@gmail.com>
2024-10-02 03:13:11 +0300

    update

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flasgger import Swagger
-from flask_restx import Api
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -10,24 +9,10 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:qwerty@localhost:5432/Blog"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123456@127.0.0.1:5432/Blog"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
-
-template = {
-    "swagger": "2.0",
-    "info": {
-        "title": "Flask Blog API",
-        "description": "API documentation for a blog application built with Flask and PostgreSQL.",
-        "version": "1.0"
-    }
-}
-app.config['SWAGGER'] = {
-    'title': 'Flask Blog API',
-    'uiversion': 2
-}
-Swagger(app, template=template)
+migrate = Migrate(app, db)
 
 from app import models , routes
